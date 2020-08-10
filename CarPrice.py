895874c90569999c863efb28225455b8d72a73c9
DakshDudeja <61489137+DakshDudeja@users.noreply.github.com>
2020-08-10 14:23:46 +0530

    Update CarPrice.py

--- CarPrice.py
+++ CarPrice.py
@@ -189,5 +189,5 @@ fig.suptitle('y_test vs y_pred', fontsize=20)              # Plot heading
 plt.xlabel('y_test', fontsize=18)                          # X-label
 plt.ylabel('y_pred', fontsize=16)   
 
-https://www.kaggle.com/arjunbhasin2013/ccdata
+
 
